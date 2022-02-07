f0d97138c8de5d015a9571e1a61d8db489c8f38e
Thushara Samaraweera <thusharadasun204@gmail.com>
2022-02-08 03:49:58 +0530

    create register and login form

--- src/App.js
+++ src/App.js
@@ -3,7 +3,24 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      
+      <div className='register-form'>
+        <h3>Register user</h3>
+        <input placeholder='Enter email' className='input'/>
+        <input placeholder='Enter password' className='input' />
+
+        <button>Create user</button>
+      </div>
+
+      <div className='login-form'>
+        <h3>Login</h3>
+        <input placeholder='Enter email' className='input'/>
+        <input placeholder='Enter password' className='input' />
+
+        <button>Login</button>
+      </div>
+
+      <h4>User logged in</h4>
+      <button> Sign out</button>
     </div>
   );
 }
